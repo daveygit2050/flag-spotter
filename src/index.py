c66f3704458f2bfaf7a07ebe62dd8686b2bad961
Dave Randall <dave@goldsquare.co.uk>
2018-04-08 21:24:09 +0100

    Function now returns the country name and code by calling a 3rd party API

--- src/index.py
+++ src/index.py
@@ -1,6 +1,22 @@
 import boto3
-import os
+import json
+from botocore.vendored import requests
 
 def handler (event, context):
-  print os.environ('TEST_STRING')
 
+  ip = event['requestContext']['identity']['sourceIp']
+  ip_api_url = "https://ipapi.co/" + ip + "/json/"
+  ip_api_response = requests.get(ip_api_url).json()
+
+  body = {
+    "country": ip_api_response['country'],
+    "country_name": ip_api_response['country_name']
+  }
+
+  response = {
+    "statusCode": 200,
+    "body": json.dumps(body)
+  }
+
+  print(response)
+  return(response)
